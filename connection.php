551b698ea5a0d73321a264db07a771256ad57ced
zeshan2001 <zeshanbh@gmail.com>
2023-12-15 14:45:53 +0300

    sql file added and connection pass changed

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-    $db = new PDO('mysql:host=localhost;dbname=poll_maker;charset=utf8', 'root','rootpass');
+    $db = new PDO('mysql:host=localhost;dbname=poll_maker;charset=utf8', 'root','');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
