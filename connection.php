c49694907e80c7e09dae139f0dbe8b2d19852a20
Hesham243 <heshamahmed243@gmail.com>
2023-12-21 18:22:50 +0300

    increase in hieght of vote page

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-    $db = new PDO('mysql:host=localhost;dbname=poll_maker;charset=utf8', 'root','rootpass');
+    $db = new PDO('mysql:host=localhost;dbname=itcs333-project-group-2;charset=utf8', 'root','rootpass');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
